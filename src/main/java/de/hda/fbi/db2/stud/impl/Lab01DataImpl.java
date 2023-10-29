63d883162dd5459d6149a7342752000481f385e0
Nils Diesterweg <nils.diesterweg@stud.h-da.de>
2023-10-29 12:29:06 +0100

    Begin implementation of Lab01Data.

---
+++ src/main/java/de/hda/fbi/db2/stud/impl/Lab01DataImpl.java
@@ -0,0 +1,22 @@
+package de.hda.fbi.db2.stud.impl;
+
+import de.hda.fbi.db2.api.Lab01Data;
+import java.util.List;
+
+public class Lab01DataImpl extends Lab01Data {
+
+  @Override
+  public List<?> getQuestions() {
+    return null;
+  }
+
+  @Override
+  public List<?> getCategories() {
+    return null;
+  }
+
+  @Override
+  public void loadCsvFile(List<String[]> csvLines) {
+
+  }
+}
