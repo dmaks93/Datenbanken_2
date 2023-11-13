18f3073f44fa0592d1049900ea5657c01f0f59d3
stdamaks <damir.maksuti@stud-h-da.de>
2023-11-13 11:43:25 +0100

    5+1 = 8

--- src/main/java/de/hda/fbi/db2/stud/impl/Lab02EntityManagerImpl.java
+++ src/main/java/de/hda/fbi/db2/stud/impl/Lab02EntityManagerImpl.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import de.hda.fbi.db2.stud.impl.Lab01DataImpl;
 
 public class Lab02EntityManagerImpl extends Lab02EntityManager {
 
