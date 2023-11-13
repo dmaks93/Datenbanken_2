fcd4d1ee87c69628b40c4a1f5c44ecd01a0f2f52
Nils Diesterweg <nils.diesterweg@stud.h-da.de>
2023-11-13 11:16:45 +0100

    Prak 2.

---
+++ src/main/java/de/hda/fbi/db2/api/Lab02EntityManagerImpl.java
@@ -0,0 +1,42 @@
+package de.hda.fbi.db2.api;
+
+import javax.persistence.EntityManager;
+
+public class Lab02EntityManagerImpl extends Lab02EntityManager {
+
+  /**
+   * Creates the {@link EntityManagerFactory} and stores it in a field. {@link #destroy()} should
+   * then clean up the factory later again.
+   */
+  @Override
+  public void init() {
+
+  }
+
+  /**
+   * {@linkplain EntityManagerFactory#close() Closes} the {@code EntityManagerFactory} created by
+   * {@link #init()}.
+   */
+  @Override
+  public void destroy() {
+
+  }
+
+  /**
+   * Here you have to persist the data in the database.
+   */
+  @Override
+  public void persistData() {
+
+  }
+
+  /**
+   * Creates a new {@code EntityManager}.
+   *
+   * @return new EntityManager
+   */
+  @Override
+  public EntityManager getEntityManager() {
+    return null;
+  }
+}
