fe2ffda26662f35c7168f1f2f075f8fbebe81bf4
Nils Diesterweg <nils.diesterweg@stud.h-da.de>
2023-11-25 18:27:14 +0100

    Fixed issues

--- src/main/java/de/hda/fbi/db2/stud/entity/Game.java
+++ src/main/java/de/hda/fbi/db2/stud/entity/Game.java
@@ -4,7 +4,19 @@ package de.hda.fbi.db2.stud.entity;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
 
 @Entity
 public class Game {
