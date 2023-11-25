f4086543ab085ec60288c817e69093de5d5e8186
stdamaks <damir.maksuti@stud-h-da.de>
2023-11-25 16:57:24 +0100

    player update

--- src/main/java/de/hda/fbi/db2/stud/entity/Player.java
+++ src/main/java/de/hda/fbi/db2/stud/entity/Player.java
@@ -1,11 +1,28 @@
 package de.hda.fbi.db2.stud.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Player {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "player_id_generator")
+    @SequenceGenerator(name = "player_id_generator", sequenceName = "db2.player_id")
     private int playerId;
+    @Column(name = "username", unique = true)
     private String username;
+
+    Player () {};
+
+    Player (String name) {
+        this.username = name;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
