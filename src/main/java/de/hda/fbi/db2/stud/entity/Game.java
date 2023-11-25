63b3a239dff50631b1ab929f5c0b4a355938078a
Nils Diesterweg <nils.diesterweg@stud.h-da.de>
2023-11-25 17:13:41 +0100

    Fixed issues

--- src/main/java/de/hda/fbi/db2/stud/entity/Game.java
+++ src/main/java/de/hda/fbi/db2/stud/entity/Game.java
@@ -9,6 +9,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -23,10 +25,9 @@ public class Game {
   private Date start;
   @Temporal(TemporalType.DATE)
   private Date end;
-  @ManyToMany
+  @ManyToOne
   private Player player;
-
-
+  @ManyToMany
   private List<Question> questionList;
 
   public Game(Player player, List<Question> questions) {
